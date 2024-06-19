d43bbfbc6150fed512ac2707cd549f42497f7c3f
SAFIR-OTTL <safir.ahamed@openturf.in>
2024-06-20 00:34:05 +0530

    Under construction page added

---
+++ src/components/UnderDev.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './UnderDev.css'
+
+function UnderDev() {
+
+  return (
+    <div className='screen_main'>
+      <div className='content1'>
+        CHAT UNITY
+        <div style={{marginTop:"10px", fontSize:"12px", letterSpacing: "11px"}}>
+          LET'S CONNECT WITH THIS WORLD
+        </div>
+      </div>
+      <div className='content2'>
+        UNDER CONSTRUCTION
+        <div style={{marginTop:"10px", fontSize:"12px", letterSpacing: "0px",fontWeight: "400"}}>
+          STAY TUNED!! And chat with anyone, anytime, anywhere
+        </div>
+      </div>
+      <div className='signature' style={{position:'absolute', bottom:"20px", right:"20px"}}>
+        -Safir Ahamed
+      </div>
+    </div>
+  )
+}
+
+export default UnderDev
---
+++ src/pages/UnderDevPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UnderDev from '../components/UnderDev'
+
+function UnderDevPage() {
+  return (
+    <div>
+        <UnderDev/>
+    </div>
+  )
+}
+
+export default UnderDevPage
